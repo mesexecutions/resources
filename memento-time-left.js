27fca22792e92067ab73924089c7a8a22f728798
Ramesh <36384808+ramepetla@users.noreply.github.com>
2023-11-24 18:40:32 +0800

    Update memento-time-left.js

--- memento-time-left.js
+++ memento-time-left.js
@@ -1,21 +1,11 @@
-// Assuming "Exclude Time" and "Schedule" are Date or DateTime fields
+var excludeTime = entry().field("ExcludeTime");
+var schedule = entry().field("Schedule");
 
-// Get the values of "Exclude Time" and "Schedule"
-var excludeTimeValue = field("Exclude Time");
-var scheduleValue = field("Schedule");
-
-// Initialize variables to store the calculated result
-var dateDifferenceDays;
-
-// Ensure both fields have values
-if (excludeTimeValue && scheduleValue) {
-    // Calculate the date difference in days
-    dateDifferenceDays = moment(scheduleValue).startOf('day').diff(moment(excludeTimeValue).startOf('day'), 'days');
+if (excludeTime && schedule) {
+    var timeDiff = schedule.getTime() - excludeTime.getTime();
+    var daysDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+    entry().set("DaysDifference", daysDiff);
 } else {
-    // If either field is empty, handle accordingly
-    dateDifferenceDays = null; // or any other default value or error handling
+    message("Please provide values for both ExcludeTime and Schedule fields.");
+    cancel();
 }
-
-// Now you can use the 'dateDifferenceDays' variable as needed
-// For example, you can display it or use it in further calculations
-dateDifferenceDays;
