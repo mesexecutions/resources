6b2c146f9b7e2257e294def871918bd5396a24de
Ramesh <36384808+ramepetla@users.noreply.github.com>
2023-11-24 18:23:51 +0800

    Update memento-time-left.js

--- memento-time-left.js
+++ memento-time-left.js
@@ -1,1 +1,11 @@
-console.log("Hello");
+// Example JavaScript script for Memento
+// This script logs a message to the console
+
+// Get the current entry
+var currentEntry = entry();
+
+// Log a message to the console
+console.log("Hello from Memento JavaScript! Current entry ID: " + currentEntry.id);
+
+// Display a notification to the user
+message("Script executed successfully!");
